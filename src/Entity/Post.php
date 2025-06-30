4578e41e52a58b613bb79df168388b793fc31095
Игнатьев Егор Валерьевич <egorps4@gmail.com>
2025-06-30 19:40:47 +0400

    add Post entity

---
+++ src/Entity/Post.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    private string $title;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private string $content;
+
+    #[ORM\Column(type: Types::FLOAT)]
+    private float $hotness;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private DateTime $createdAt;
+
+    #[ORM\Column(type: Types::INTEGER)]
+    private int $viewCount = 0;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false, name: 'author_id')]
+    private User $author;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    public function getHotness(): float
+    {
+        return $this->hotness;
+    }
+
+    public function setHotness(float $hotness): self
+    {
+        $this->hotness = $hotness;
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getViewCount(): int
+    {
+        return $this->viewCount;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $user): self
+    {
+        $this->author = $user;
+        return $this;
+    }
+
+    public function incrementViewCount(): self
+    {
+        $this->viewCount++;
+        return $this;
+    }
+}
