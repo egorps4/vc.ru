6557af8ccdd145674b8f742007a97476e09d9f83
Игнатьев Егор Валерьевич <egorps4@gmail.com>
2025-07-02 00:04:34 +0400

    expand fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Post;
 use App\Entity\User;
+use App\Entity\UserView;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -14,15 +15,47 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create();
 
-        $userNames = ['Адам Дженсен', 'Granger', 'Вадим', 'Габен', 'Гервант', 'Егор', 'Анжелика', 'Натан Драке', 'Думгай', 'Снейк'];
+        $userFirstNames = [
+            'Адам',
+            'Granger',
+            'Дум',
+            'Гейб',
+            'Геральт',
+            'Егор',
+            'Лара',
+            'Натан',
+            'Джей Си',
+            'Солид',
+            'Хорус',
+            'Панам',
+            'Трисс'
+        ];
+
+        $userLastNames = [
+            'Дженсен',
+            'Сосиска',
+            'Гай',
+            'Ньюэлл',
+            'из Ривии',
+            'Игнатьев',
+            'Крофт',
+            'Драке',
+            'Дентон',
+            'Снейк',
+            'Лупрекаль',
+            'Палмер',
+            'Меригольд'
+        ];
 
         $users = [];
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $user = new User();
-            $user->setName($userNames[$i]);
+            $user->setName($faker->randomElement($userFirstNames) . ' ' . $faker->randomElement($userLastNames));
             $manager->persist($user);
             $users[] = $user;
         }
+
+        $manager->flush();
 
         $gameTopics = [
             'Обзор новой игры {game}',
@@ -30,20 +63,28 @@ class AppFixtures extends Fixture
             'Вы НЕ поняли {game}',
             'Почему {game} плохая игра',
             'Я прошел {game} и это шедевр',
+            'Утечка: {game} выйдет до конца текущего года',
+            '{game} получила патч первого дня на 1000 ГБ',
+            '{game} не повторит ошибок предшественницы - игра не выйдет'
         ];
-        $games = ['Cyberpunk 2077', 'Deus Ex', 'The Witcher 3', 'Baldur\'s Gate 3', 'Doom'];
+        $games = ['Cyberpunk 2078', 'Deus Ex Icarus', 'The Witcher 4', 'Baldur\'s Gate 4', 'Doom final', 'Metal Gear Solid 6', 'Half-Life 3'];
 
-        for ($i = 0; $i < 5000; $i++) {
+        for ($i = 0; $i < 4000; $i++) {
             $post = new Post();
             $game = $faker->randomElement($games);
             $topic = str_replace('{game}', $game, $faker->randomElement($gameTopics));
             $post->setTitle($topic);
-            $post->setContent($faker->paragraphs(3, true));
+            $post->setContent($faker->paragraphs(1, true));
             $post->setHotness($faker->randomFloat(2, 0, 100));
             $post->setCreatedAt($faker->dateTimeBetween('-1 year', 'now'));
             $post->setAuthor($faker->randomElement($users));
             $post->setViewCount($faker->numberBetween(0, 1500));
             $manager->persist($post);
+
+            $userView = new UserView();
+            $userView->setPost($post);
+            $userView->setUser(($faker->randomElement($users)));
+            $manager->persist($userView);
         }
 
         $manager->flush();
